72d0b68efb73c16d4dbd56e099f698a5b9503242
Elitecrafttech <Elitecrafttech@gmail.com>
2024-06-29 11:28:14 -1100

    js commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function menu(){
+    const togle = document.querySelector(".togle")
+    togle.style.display = "flex"
+}
+function closemenu(){
+    const togle = document.querySelector(".togle")
+    togle.style.display = "none"
+}
